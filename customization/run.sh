a06d22e95db7b4ddef9cee75f54cdad9fa3b461e
havenwang <Haven.Wang@rackspace.com>
2015-07-13 16:56:22 -0500

    add force install and ensure not to overwrite existing sphinx dir

--- customization/run.sh
+++ customization/run.sh
@@ -13,17 +13,20 @@ while [[ $# > 0 ]] ; do
 			NAME="$2"; shift;;
 	    -d|--destination)
 			DESTINATION="$2"; shift;;
+		-f|--force-install)
+			FORCEINSTALL = true;;
 	    -h|--help)
 	    	echo "usage: ./run.sh [OPTIONS] [ARGUMENTS]"
 	    	echo "=====OPTIONS====="
 	    	echo "-h, --help : Displays this output"
-	    	echo "-n, --name : REQUIRED. Name of the project"
+	    	echo "-n, --name : REQUIRED. Name of the project directory where the code lives"
 	    	echo "-d, -destination: Name of the directory to contain files needed for documentation. Set to /docs/ by default."
 	    	exit 0;;
 	    --default)
 	    	DEFAULT=YES;;
 	    *)
-	    	echo "ERROR: Invalid option: \""$opt"\"" >&2
+	    	echo "ERROR: Invalid option: \""$opt"\"" 
+	    	echo "run `./run.sh --help` to see usage.">&2
 	    	exit 1;;
 	esac
 	shift
@@ -44,10 +47,17 @@ if [[ -n $1 ]]; then
     tail -1 $1
 fi
 
-#download Sphinx into a docs directory
+#create directory for docs files
 mkdir $DESTINATION
 cd $DESTINATION
-git clone https://github.com/sphinx-doc/sphinx.git
+
+#check if sphinx directory already exists before cloning
+if [ ! -d "sphinx" -o "$FORCEINSTALL" = true ]; then
+	git clone https://github.com/sphinx-doc/sphinx.git
+else
+	echo "WARNING: sphinx directory already exists here. If you wish to overwrite existing directory, execute the script using the '--force-install' option." >&2
+	exit 1
+fi
 
 #download and replace their apidoc.py with mine
 cd sphinx/sphinx/
@@ -63,14 +73,15 @@ cd ../
 echo "Follow instructions to get Sphinx set up. You may stick with the default selections for the entries, but just be sure to say YES to getting the autodoc extension."
 sphinx-quickstart
 
-#let's go ahead and delete sphinx directory to minimze clutter
-rm -rf sphinx/
-
 #add path to where we reference the modules from to conf.py
 echo "sys.path.insert(0, os.path.abspath('../'))" >> conf.py
 
 #run sphinx-apidoc
 sphinx-apidoc --force --module-first -o ./ ../$NAME
+
+#let's go ahead and delete sphinx directory to minimze clutter
+#remember: if we've made it down here then we know from earlier that sphinx/ dir didn't exist yet
+rm -rf sphinx/
 
 #set index.rst to what's in modules.rst
 mv modules.rst index.rst
