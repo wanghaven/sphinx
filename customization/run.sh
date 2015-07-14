428278fcf61185bcb36bd0b9b685c3c418468558
havenwang <Haven.Wang@rackspace.com>
2015-07-14 14:19:16 -0500

    change message for default script execution

--- customization/run.sh
+++ customization/run.sh
@@ -18,8 +18,7 @@ while [[ $# > 0 ]] ; do
 			QUICKSTART=true;;
 	    -h|--help)
 	    	echo "usage: ./run.sh [OPTIONS] [ARGUMENTS]"; echo
-	    	echo "Simplest (and recommended usage) is to run './run.sh -f -p PROJECT -a AUTHOR -v VERSION'"
-	    	echo "Note: please use the -f option carefully, it will overwrite an existing sphinx directory if needed."
+	    	echo "Simplest (and recommended usage) is to run './run.sh -p PROJECT -a AUTHOR -v VERSION'"
 	    	echo
 	    	echo "=====OPTIONS====="
 	    	echo "-h, --help : Displays this output. Note: when this argument is supplied no execution will occur."
